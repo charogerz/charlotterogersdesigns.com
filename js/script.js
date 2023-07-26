ba134210e4a3e5e1fe45fda78bf9095d15d393e3
charogerz <charogerz@gmail.com>
2023-07-26 16:18:26 -0400

    trying navbar disappear again

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}

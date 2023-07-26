1ce812a3879eb3b52a00a6256eac467c653d2410
charogerz <charogerz@gmail.com>
2023-07-26 17:09:52 -0400

    attempt with <script> element

--- js/script.js
+++ js/script.js
@@ -3,16 +3,3 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
-
-var lastScrollTop;
-navbar = document.getElementById('navbar');
-window.addEventListener('scroll',function(){
-var scrollTop = window.scrollY || document.documentElement.scrollTop;
-if(scrollTop > lastScrollTop){
-navbar.style.top='-80px';
-}
-else{
-navbar.style.top='0';
-}
-lastScrollTop = scrollTop;
-});

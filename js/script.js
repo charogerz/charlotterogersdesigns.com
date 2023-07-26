68c85277de5af67eb1983fcd12c69c6e894d373f
charogerz <charogerz@gmail.com>
2023-07-26 16:06:14 -0400

    attempting navbar disappearance

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+var didScroll;
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+$(window).scroll(function(event){
+  didScroll = true;
+});
+
+setInterval(function() {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+function hasScrolled() {
+  var st = $(this).scrollTop();
+  if (Math.abs(lastScrollTop-st) <= delta) {
+    return;
+  }
+  if (st > lastScrollTop && st > navbarHeight) {
+    $('header').removeClass('nav-down').addClass('nav-up');
+  } else {
+    if(st + $(window).height() < $(document).height()) {
+      $('header').removeClass('nav-up').addClass('nav-down');
+    }
+  }
+  lastScrollTop = st;  
+}
+
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
